6fc998e719c2ad7f50767d22a470661e8eb43a41
Petter <petter.reijalt@gmail.com>
2025-11-06 11:16:26 +0100

    hallo

--- src/myproject/main.py
+++ src/myproject/main.py
@@ -7,11 +7,13 @@ This module provides:
 from myproject import __version__
 
 
-def foo1(a):
-    c = a + 1
+def foo2(a):
+  b = 0
+  c = a + 1  
+  if a > 0:
     if a > 10:
-        return 10
-    return a + c
+      return 10
+  return a + c
 
 
 def add(a: int, b: int) -> int:
