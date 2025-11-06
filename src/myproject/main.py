68e2e49fdcf943fc055247a3b607a17ffd13b805
Petter <petter.reijalt@gmail.com>
2025-11-06 11:18:31 +0100

    noqa

--- src/myproject/main.py
+++ src/myproject/main.py
@@ -7,6 +7,15 @@ This module provides:
 from myproject import __version__
 
 
+def foo3(a):
+    b = 0  # noqa: F841
+    c = a + 1
+    if a > 0:  # noqa: SIM102
+        if a > 10:
+            return 10
+    return a + c
+
+
 def add(a: int, b: int) -> int:
     """Adds two positive numbers together.
 
